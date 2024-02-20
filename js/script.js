e523be5a964ff515187ded6df1401b65b94d42b3
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-20 19:16:20 +0100

    piccole modifiche

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Bonus:
-// 1- oltre al click sul pulsante per la aggiunta del task, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 // 2- cliccando sul testo dell'item, invertire il valore della proprietà done del to-do corrispondente (se done era uguale a false, impostare true e viceversa)
 
 const {createApp} = Vue;
