923e992f91b82480a1377e40c4722c2194b4be74
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-20 18:17:54 +0100

    milestone 1 e 2 completati

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto :temporale_nuvola_e_pioggia:, formato da due proprietà:
-// text, una stringa che indica il testo del todo
-// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una ":x:": cliccando su di essa, il todo viene rimosso dalla lista.
 // MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo to-do, che quindi viene aggiunto alla lista dei to-do esistenti.
 // Bonus:
 // 1- oltre al click sul pulsante per la aggiunta del task, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del to-do corrispondente (se done era uguale a false, impostare true e viceversa)
 
 const {createApp} = Vue;
 
@@ -19,7 +10,32 @@ createApp({
     data() {
         return {
             
-
+            tasks: [
+                {
+                  text: "Comprare latte",
+                  done: false,
+                },
+                {
+                  text: "Chiamare l'elettricista",
+                  done: false,
+                },
+                {
+                  text: "Preparare la presentazione",
+                  done: false,
+                },
+                {
+                  text: "Fare la spesa",
+                  done: false,
+                },
+                {
+                  text: "Pagare le bollette",
+                  done: false,
+                },
+                {
+                  text: "Portare l'auto dal meccanico",
+                  done: false,
+                }
+              ],
 
 
 
@@ -30,9 +46,17 @@ createApp({
 
     methods: {
     
-        
+        taskDone(index) {
+            this.tasks[index].done = true;
+        },
+
+        deleteTask(taskIndex) {
+            this.tasks.splice(taskIndex, 1);
+        }
 
 
     },
 
 }).mount("#app");
+
+// v-if="tasks.done = true ? 'del': '' "
