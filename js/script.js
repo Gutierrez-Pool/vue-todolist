e17bccb9472e7cbb7310c15a5d1c397d743b4f12
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-20 18:57:17 +0100

    bonus 1 completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo to-do, che quindi viene aggiunto alla lista dei to-do esistenti.
 // Bonus:
 // 1- oltre al click sul pulsante per la aggiunta del task, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 // 2- cliccando sul testo dell'item, invertire il valore della proprietà done del to-do corrispondente (se done era uguale a false, impostare true e viceversa)
@@ -66,8 +64,7 @@ createApp({
             this.tasks.push(newTask)
 
             this.newTask = '';
-        }
-
+        },
 
     },
 
